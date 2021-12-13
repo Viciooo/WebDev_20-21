6460abd57711089d7fb3a25e2c301b4ec583ef85
Viciooo <72527323+Viciooo@users.noreply.github.com>
2021-12-13 11:30:50 +0100

    rebuild

--- lab5/zad1/src/app/app.module.ts
+++ lab5/zad1/src/app/app.module.ts
@@ -1,32 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
+import { PostsComponent } from './posts/posts.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent
+    PostsComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
